e8ee2c631e6c763f491c62ba1488728dcdb1ea3a
LijinWilson <lijinwilson2018@gmail.com>
2024-02-07 12:44:11 +0530

    completed the intro section

--- components/intro.tsx
+++ components/intro.tsx
@@ -3,6 +3,10 @@
 import Image from "next/image";
 import React from "react";
 import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { FaGithubSquare } from "react-icons/fa";
 
 export default function Intro() {
   return (
@@ -27,7 +31,7 @@ export default function Intro() {
             }}
           >
             <Image
-              src="https://content.wepik.com/statics/21209533/preview-page0.jpg"
+              src="/Lijin-Wilson.jpg"
               alt="Lijin Wilson"
               width="192"
               height="192"
@@ -59,13 +63,53 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Lijin Wilson.</span> I'm a{" "}
+        <span className="font-bold">Hello, I am Lijin Wilson.</span> I am a{" "}
         <span className="font-bold">Web developer</span> I enjoy building{" "}
         <span className="italic">sites</span>. My focus is{" "}
         <span className="underline">React (Next.js) Angular</span>.
       </motion.p>
 
       {/* Contact me, download CV, Linkedin, Git hub section */}
+      <motion.div
+        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.1 }}
+      >
+        {/* contact me here */}
+        <Link
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 "
+        >
+          Contact me here{" "}
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+        </Link>
+        {/* download cv */}
+        <a
+          className="group flex items-center gap-2 bg-white rounded-full px-7 py-3 outline-none focus:scale-110 hover:scale-105  active:scale-105 transition cursor-pointer border border-black/10"
+          href="/Lijin-Wilson.pdf"
+          download
+        >
+          Download CV{" "}
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+        </a>
+        {/* Linkedin */}
+        <a
+          className="flex items-center gap-2 bg-white text-gray-700 rounded-full p-4 focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer border border-black/10"
+          href="https://www.linkedin.com/in/lijinwilson/"
+          target="_blank"
+        >
+          <BsLinkedin />
+        </a>
+        {/* github */}
+        <a
+          className="flex items-center gap-2 bg-white text-gray-700 rounded-full p-4 focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 text-[1.35rem] transition cursor-pointer border border-black/10"
+          href="https://github.com/LijinWilson"
+          target="_blank"
+        >
+          <FaGithubSquare />
+        </a>
+      </motion.div>
     </section>
   );
 }
